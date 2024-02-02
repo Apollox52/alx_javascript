04444e2d81b6d08dd73da89544386b0b942a52f9
mouad <mr.robott52@gmail.com>
2024-02-02 19:34:31 +0100

    Hey11

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+// Use const or let to declare variables (let is used here)
+let line1 = "C is fun";
+let line2 = "Python is cool";
+let line3 = "JavaScript is amazing";
+
+// Print each line using console.log
+console.log(line1);
+console.log(line2);
+console.log(line3);
