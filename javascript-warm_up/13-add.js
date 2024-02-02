49393610ddaf65602343e48b3377c76e7c22ffc0
mouad <mr.robott52@gmail.com>
2024-02-02 20:08:30 +0100

    Hey11

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// Function to add two integers
+function add(a, b) {
+  return a + b;
+}
+
+// Export the add function to make it visible outside the module
+module.exports = { add };
