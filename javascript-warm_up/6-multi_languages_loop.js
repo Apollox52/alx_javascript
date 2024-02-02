440d24b3aaf0f01f14645b9f070da4f9e7d9ca87
mouad <mr.robott52@gmail.com>
2024-02-02 19:57:44 +0100

    Hey11

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Use a for loop to iterate through the array and print each line
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
