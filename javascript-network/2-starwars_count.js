95dfa408984e3064b5979c8b77b44556ea6e0b19
mouad <mr.robott52@gmail.com>
2024-02-08 20:14:43 +0100

    Hey11

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,31 +1,13 @@
-const request = require('request');
+const fetch = require('node-fetch');
 
-// Get the API URL from the command line argument
-const apiUrl = process.argv[2];
+const apiURL = process.argv[2];
 
-// Character ID for Wedge Antilles
-const characterId = 18;
-
-// Make a GET request to the API URL
-request(apiUrl, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-        console.log('Error fetching data from API');
-    } else {
-        // Parse the JSON response
-        const films = JSON.parse(body);
-
-        // Initialize a counter for movies where Wedge Antilles is present
-        let count = 0;
-
-        // Iterate through each film
-        films.forEach(film => {
-            // Check if Wedge Antilles is present in the characters array of the film
-            if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-                count++;
-            }
-        });
-
-        // Print the count of movies where Wedge Antilles is present
+fetch(apiURL)
+    .then(response => response.json())
+    .then(data => {
+        const films = data.results;
+        const count = films.filter(film => film.characters.includes("https://swapi-api.alx-tools.com/api/people/18/")).length;
         console.log(count);
-    }
-});
+    })
+    .catch(error => console.error('Error:', error));
+
