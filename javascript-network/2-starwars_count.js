c1725dc8d659ca57777dc424de8fc651ed49972d
mouad <mr.robott52@gmail.com>
2024-02-08 20:15:55 +0100

    Hey11

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,13 +1,15 @@
-const fetch = require('node-fetch');
+const request = require('request');
 
 const apiURL = process.argv[2];
 
-fetch(apiURL)
-    .then(response => response.json())
-    .then(data => {
-        const films = data.results;
-        const count = films.filter(film => film.characters.includes("https://swapi-api.alx-tools.com/api/people/18/")).length;
-        console.log(count);
-    })
-    .catch(error => console.error('Error:', error));
-
+request(apiURL, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+        return;
+    }
+    
+    const data = JSON.parse(body);
+    const films = data.results;
+    const count = films.filter(film => film.characters.includes("https://swapi-api.alx-tools.com/api/people/18/")).length;
+    console.log(count);
+});
