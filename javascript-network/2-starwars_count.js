cc8fc44de8b3ca14ca49a71fd6808f62d32fca8f
mouad <mr.robott52@gmail.com>
2024-02-08 17:50:34 +0100

    Hey11

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,29 +1,21 @@
-// Import the request module
 const request = require('request');
 
-// Get the API URL from the command line argument
 const apiUrl = process.argv[2];
-
-// Define the character ID for Wedge Antilles
 const characterId = '18';
 
-// Make a GET request to the provided API URL
 request(apiUrl, (error, response, body) => {
-    // Parse the JSON response body
-    const films = JSON.parse(body).results;
+    if (error || response.statusCode !== 200) {
+        console.log('Error fetching movie data');
+    } else {
+        const films = JSON.parse(body).results;
+        let movieCount = 0;
 
-    // Initialize a counter for the number of movies with Wedge Antilles
-    let movieCount = 0;
+        films.forEach(movie => {
+            if (movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+                movieCount++;
+            }
+        });
 
-    // Loop through each movie in the results
-    films.forEach(movie => {
-        // Check if the characters array includes Wedge Antilles
-        if (movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-            // Increment the movie count if Wedge Antilles is present
-            movieCount++;
-        }
-    });
-
-    // Print the number of movies with Wedge Antilles
-    console.log(movieCount);
+        console.log(movieCount);
+    }
 });
